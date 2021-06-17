6a1b72b7c539128ce254895d35df1aeb8f0d5012
YR <yuroutan17@gmail.com>
2021-06-17 14:52:04 +0800

    Initial Commit

---
+++ Test Project/ContentView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+            Text("Goodbye, world!")
+            Text("I'm a little app for your big needs!")
+            Button("Learn more") {
+            
+            }
+        }
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test Project/Test_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_ProjectApp.swift
+//  Test Project
+//
+//  Created by rgs on 16/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Test_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
